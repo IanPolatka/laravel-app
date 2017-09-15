9a0274ecb394fbc4ee41a391a7a85bb6588ecc5e
Ian Polatka <ianpolatka@Ians-MacBook-Pro.local>
2017-09-15 14:01:21 -0400

    Home page now includes all football game from that day.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Carbon\Carbon;
+
+use App\Football;
+use App\Team;
+use App\Time;
+
+use DB;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +31,67 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $away_team_score_computed =     \DB::table('football')
+                    ->select(\DB::raw('sum(
+                                IFNULL( `football`.`away_team_first_qrt_score` , 0 ) +
+                                IFNULL( `football`.`away_team_second_qrt_score` , 0 ) +
+                                IFNULL( `football`.`away_team_third_qrt_score` , 0 ) +
+                                IFNULL( `football`.`away_team_fourth_qrt_score` , 0 ) +
+                                IFNULL( `football`.`away_team_overtime_score` , 0 )
+                                )
+                                AS score' 
+                    ))
+                    ->get()->pluck('score')->first();
+
+        $home_team_score_computed =     \DB::table('football')
+                    ->select(\DB::raw('sum(
+                                IFNULL( `football`.`home_team_first_qrt_score` , 0 ) +
+                                IFNULL( `football`.`home_team_second_qrt_score` , 0 ) +
+                                IFNULL( `football`.`home_team_third_qrt_score` , 0 ) +
+                                IFNULL( `football`.`home_team_fourth_qrt_score` , 0 ) +
+                                IFNULL( `football`.`home_team_overtime_score` , 0 )
+                                )
+                                AS score' 
+                    ))
+                    ->get()->pluck('score')->first();
+
+        $football = Football::join('teams as home_team', 'football.home_team_id', '=', 'home_team.id')
+                            ->join('teams as away_team', 'football.away_team_id', '=', 'away_team.id')
+                            ->join('times', 'football.time_id', '=', 'times.id')
+                            ->select(
+                                'football.id',
+                                'away_team.school_name as away_team',
+                                'away_team.abbreviated_name as away_team_abbreviated_name',
+                                'away_team.mascot as away_team_mascot',
+                                'away_team.logo as away_team_logo',
+                                'away_team.city as away_team_city',
+                                'away_team.state as away_team_state',
+                                'home_team.school_name as home_team',
+                                'home_team.abbreviated_name as home_team_abbreviated_name',
+                                'home_team.mascot as home_team_mascot',
+                                'home_team.logo as home_team_logo',
+                                'home_team.city as home_team_city',
+                                'home_team.state as home_team_state',
+                                'football.game_status',
+                                'time',
+                                'away_team_first_qrt_score',
+                                'away_team_second_qrt_score',
+                                'away_team_third_qrt_score',
+                                'away_team_fourth_qrt_score',
+                                'away_team_overtime_score',
+                                'away_team_final_score',
+                                'home_team_first_qrt_score',
+                                'home_team_second_qrt_score',
+                                'home_team_third_qrt_score',
+                                'home_team_fourth_qrt_score',
+                                'home_team_overtime_score',
+                                'home_team_final_score'
+                            )
+                            ->whereDate('date', Carbon::today())
+                            ->get();
+
+
+        return view('home', compact('football'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -13,5 +13,112 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-lg-12">
+
+        @if (Auth::user())
+
+            <h3><strong>Welcome {{ Auth::user()->name }},</strong></h3>
+
+            <p>Here are today's football events</p>
+
+        @endif
+
+        <ul class="home-events">
+
+            @foreach($football as $item)
+
+            <?php 
+
+            $awayTeamScore = $item->away_team_first_qrt_score + 
+                                   $item->away_team_second_qrt_score + 
+                                   $item->away_team_third_qrt_score +
+                                   $item->away_team_fourth_qrt_score +
+                                   $item->away_team_overtime_score;
+
+            $homeTeamScore = $item->home_team_first_qrt_score + 
+                                   $item->home_team_second_qrt_score + 
+                                   $item->home_team_third_qrt_score +
+                                   $item->home_team_fourth_qrt_score +
+                                   $item->home_team_overtime_score;
+
+            ?>
+
+            @if (($item->game_status > 1) && ($item->game_status < 7)) 
+                <li class="red-pulse-border">
+            @else
+                <li>
+            @endif
+            <a href="/football/game/{{ $item->id }}">
+                <div class="team">
+                    @if ($item->away_team_logo)
+                        <img src="/images/team-logos/{{ $item->away_team_logo }}">
+                    @endif
+                    {{$item->away_team}}
+                    <span class="score">
+                        @if ($item->game_status == NULL || $item->game_status < 1)
+                            -
+                        @else
+                            <?php echo $awayTeamScore; ?>
+                        @endif
+                    </span>
+                </div>
+                <div class="team">
+                    @if ($item->home_team_logo)
+                        <img src="/images/team-logos/{{ $item->home_team_logo }}">
+                    @endif
+                    {{$item->home_team}}
+                    <span class="score">
+                        @if ($item->game_status == NULL || $item->game_status < 1)
+                            -
+                        @else
+                            <?php echo $homeTeamScore; ?>
+                        @endif
+                    </span>
+                </div>
+
+                @if (($item->game_status > 1) && ($item->game_status < 7))
+                    <div class="red-pulse-background status">
+                @else
+                    <div class="status">
+                @endif
+                    @if($item->game_status > 1 )
+                        @if ($item->game_status == 1) 
+                            1st Quarter
+                        @endif
+                        @if ($item->game_status == 2) 
+                            2nd Quarter 
+                        @endif
+                        @if ($item->game_status == 3) 
+                            Halftime
+                        @endif
+                        @if ($item->game_status == 4) 
+                            3rd Quarter
+                        @endif
+                        @if ($item->game_status == 5) 
+                            4th Quarter
+                        @endif
+                        @if ($item->game_status == 6) 
+                            Overtime
+                        @endif
+                    @else
+                        {{$item->time}}
+                    @endif
+                </div><!--  Status  -->
+            </a></li>
+
+            @endforeach
+
+        </ul>
+
+    </div>
+
+    </div><!--  Row  -->
+
+</div><!--  Container  -->
+
 @endsection
